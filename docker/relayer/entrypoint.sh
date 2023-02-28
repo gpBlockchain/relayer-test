94d1fcf4e26b70c08259e3f2dc7e1716404f8ef7
gpBlockchain <744158715@qq.com>
2023-02-28 16:01:57 +0800

    add herme

--- docker/relayer/entrypoint.sh
+++ docker/relayer/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -e
-exce mkdir ~/.hermes/
-exec cp config.toml ~/.hermes/
-exec RUST_BACKTRACE=1 RUST_LOG=trace ./hermes forcerelay --ethereum-chain-id ibc-eth-0 --ckb-chain-id ibc-ckb-0
+mkdir ~/.hermes/
+cp config.toml ~/.hermes/
+export RUST_BACKTRACE=1 RUST_LOG=trace
+exec hermes forcerelay --ethereum-chain-id ibc-eth-0 --ckb-chain-id ibc-ckb-0
