e46a32f377cd96fd1993dac2b073ce38e8597ca2
iu小迷弟 <15168316096@163.com>
2023-03-02 03:33:57 +0000

    update config.test.ts

--- tests/config.test.ts
+++ tests/config.test.ts
@@ -1,25 +1,28 @@
 import {step} from "./utils/util";
-import {sh} from "../src/utils/sh";
 import * as fs from "fs";
 import * as child_process from 'child_process';
-// import path from 'path';
+import { assert } from "chai";
 
 describe('config chainid is error', function () {
     const path = require('path');
     const rootPath = path.resolve(__dirname, '..');
+    console.log(rootPath);
     const configVerifier = path.join(rootPath, 'build/data/verify/helios.toml');
     const startCmd = path.join(rootPath, 'build/start.sh');
     const configVerifierContent = fs.readFileSync(configVerifier, "utf-8");
     it("id is not exist on chain", async () => {
-        await step("modify config && start verfier serv", async () => {
+        await step("1.modify config", async () => {
             console.log("modify config");
             const ModifyConfigVerifier = child_process.execSync(`echo "${configVerifierContent}" | sed 's/ckb_ibc_client_id = "ibc-ckb-0"/ckb_ibc_client_id = "ibc-ckb-error"/'`);
             const tempModifyConfigVerifier = 'temp.toml';
             fs.writeFileSync(tempModifyConfigVerifier, ModifyConfigVerifier, 'utf-8');
             const outputFilename = 'helios.toml';
             fs.renameSync(tempModifyConfigVerifier, outputFilename);
+        })
+        await step("2.start verfier serv", async () => {
             console.log("start vrefier serv");
-            await sh(`bash ${startCmd}`);
+            const keyWord = child_process.execSync(`bash ${startCmd}`);
+            assert(keyWord.toString('utf-8') === "no configuration file provided: not found", "check config error not found key words");
         })
     })
 });
