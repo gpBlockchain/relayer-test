5685fd45ba588fef7d3f59f6c180c5bf6f3bb90c
gpBlockchain <744158715@qq.com>
2023-03-14 10:47:57 +0800

    add error msg

--- tests/config.test.ts
+++ tests/config.test.ts
@@ -9,7 +9,9 @@ describe('config chainid is error', function () {
     it("id is not exist on chain, expect Error: forcerelay error: no lightclient cell deployed on ckb", async () => {
         await step("1.start verifier serv", async () => {
             const keyWord = await sh(`cd ${startCmdPath} && bash start.sh`);
-            expect(JSON.stringify(keyWord)).to.be.include("Error: forcerelay error: no lightclient cell deployed on ckb",JSON.stringify(keyWord));
+            //forcerelay error: no lightclient cell deployed on ckb
+            //"Error: forcerelay error: no lightclient cell deployed on ckb"
+            expect(JSON.stringify(keyWord)).to.be.include("no lightclient cell deployed on ckb",JSON.stringify(keyWord));
         })
     })
 });
